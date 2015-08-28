0d9e7c8132f4278bb30d5a460ca361df8bd74ea6
saikitchan <saikitchan@outlook.com>
2015-08-28 16:37:50 -0400

    change to slice

--- src/Problem_10.py
+++ src/Problem_10.py
@@ -6,19 +6,19 @@ Created on Aug 27, 2015
 
 
 import time 
+import itertools
 start_time = time.time()    
 
 def sumPrime(limit):   
     primeList=[True] * limit 
     total=2
     for i in range(3, int(limit**0.5)+1,2):
-        if primeList[i]:
-            for x in range(i*2,limit,i):
-                primeList[x]=False 
+        if primeList[i]: 
+            primeList[i*i::2*i]  = [False] * int((limit-i*i-1)/(2*i)+1) 
     for i in range(3,limit,2):
         if primeList[i]:
             total+=i
     return total
  
-print(sumPrime(1000000)) 
+print(sumPrime(2000000)) 
 print( (time.time() - start_time), "seconds" )
